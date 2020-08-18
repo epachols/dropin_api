8797ce73f0ef672fd7a7706a070368f35bd5d3c2
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-18 10:19:46 -0700

    added bcrypt and user login route

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models")
+const bcrypt = require('bcrypt')
 //all routes here will start with /api/users
 
 
 //for creating a new user
-router.post("/", (req, res) => {
+router.post("/signup", (req, res) => {
     //TODO: enable session user check when login authentication getting up and running.
     // if(!req.session.user){
     //     res.status(401).send("login required")
@@ -28,13 +29,21 @@ router.post("/", (req, res) => {
     // }
 })
 
-//This route finds a single user by their ID - useful for after login
-router.get("/:id", (req, res) => {
+//This route finds a single user their email
+router.post("/login", (req, res) => {
     db.User.findOne({
-        where: {id:req.params.id}
+        where: {email: req.body.email}
         //TODO: change to req.session.id after we save a session object at login. 
-    }).then(foundUser =>{
-        res.json(foundUser)
+    }).then(user =>{
+        if(!user) {
+            res.status(404).send("no such user exists")
+        } else {
+            if(bcrypt.compareSync(req.body.password,user.password)) {
+                res.send("logged in")
+            } else {
+                res.send("wrong password")
+            }
+        }
     }).catch(err => {
         console.log(err);
         res.status(500).end();
--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define('User', {
         name:DataTypes.STRING,
@@ -14,5 +16,10 @@ module.exports = function(sequelize, DataTypes) {
 //         // ex:Hall.hasMany(models.Room);
 //     };
 
+    //password encryption beforecreate hook
+    User.beforeCreate(function(user){
+        user.password= bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+    });
+    
     return User;
 };
