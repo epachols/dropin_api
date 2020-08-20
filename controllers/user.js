4cf7cd43fd001311c1bb65ee4bacd1ccce8792e6
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-20 10:18:33 -0700

    saving commits before going offline for a second

--- controllers/user.js
+++ controllers/user.js
@@ -11,11 +11,6 @@ const session = require("express-session");
 
 //for creating a new user
 router.post("/signup", (req, res) => {
-  //TODO: PUT THIS IN THE HALL ONE BUT NOT USER HERE enable session user check when login authentication getting up and running.
-  // if(!req.session.user){
-  //     res.status(401).send("login required")
-  // }
-  // else {
   db.User.create({
     name: req.body.name,
     email: req.body.email,
