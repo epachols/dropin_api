9ec3a34300635120f5df7f498d9cd6e238f4837d
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-08-19 19:11:29 -0700

    connected signin redirect and fleshed out understanding of session object

--- controllers/user.js
+++ controllers/user.js
@@ -1,68 +1,76 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models")
-const bcrypt = require('bcrypt')
-const session = require('express-session')
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 //all routes here will start with /api/users
 
+// router.get("/:id", (req,res)=>{
+//     //db.User.findOne({ADDWHEREHERE}).then(foundUser =>{DOSOMETHINGWITHIT,RESJSON}).catch(err=>{console.log(err);res.status(500).end()})
+// })
 
 //for creating a new user
 router.post("/signup", (req, res) => {
-    //TODO: PUT THIS IN THE HALL ONE BUT NOT USER HERE enable session user check when login authentication getting up and running.
-    // if(!req.session.user){
-    //     res.status(401).send("login required")
-    // }
-    // else {
-        db.User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            description: req.body.description   
-        }).then(newUser => {
-            res.json(newUser)
-        }).catch(err => {
-            console.log(err);
-            res.status(500).end()
-        })
-    // }
-})
+  //TODO: PUT THIS IN THE HALL ONE BUT NOT USER HERE enable session user check when login authentication getting up and running.
+  // if(!req.session.user){
+  //     res.status(401).send("login required")
+  // }
+  // else {
+  db.User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    description: req.body.description,
+  })
+    .then((newUser) => {
+      res.json(newUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+  // }
+});
 
 //This route finds a single user by their email to log them in
 router.post("/login", (req, res) => {
-    db.User.findOne({
+  db.User.findOne({
+    where: { email: req.body.email },
+  })
+    .then((user) => {
+      if (!user) {
+        res.status(401).send("wrong username or password");
+      } else {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          req.session.user = {
+            name: user.name,
+            email: user.email,
+            id: user.id,
+          };
 
-        where: {email: req.body.email}
-
-    }).then(user =>{
-
-        if(!user) {
-            res.status(404).send("no such user exists")
+          res.send({ user: req.session.user });
         } else {
-            if(bcrypt.compareSync(req.body.password,user.password)) {
-                req.session.user = {
-                    name:user.name,
-                    email:user.email,
-                    id:user.id
-                }
-
-                console.log("req.session", req.session)
-
-                res.send("logged in")
-            } else {
-                res.status(401).send("wrong password")
-            }
+          res.status(401).send("wrong username or password");
         }
-    }).catch(err => {
-        console.log(err);
-        res.status(500).end();
+      }
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
 
-//ROUTE FOR USER HOME PAGE
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.send("logout complete!");
+});
 
-router.get('/readsessions', (req,res)=>{
-    res.json(req.session);
-})
+router.get("/readsessions", (req, res) => {
+  if (req.session.user) {
+    res.json({ user: req.session.user });
+  } else {
+    res.json({});
+  }
+});
 
-
-module.exports = router
+module.exports = router;
